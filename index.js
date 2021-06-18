5c39bd5cdce826669b71566178a817702edaca18
elenajud <elenajud@mail.ru>
2021-06-18 13:53:23 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const port = process.env.PORT || 8080
+
+app.use(express.urlencoded({ extended: true }))
+
+app.get('/', (req, res) =>
+{
+  res.render('index.ejs')
+})
+
+app.post('/', (req, res) =>
+{
+    const username = req.body.username
+    res.render('hello.ejs', {username: username})
+}
+)
+app.listen(port, ()=> console.log(`listen port:${port}`))
+
